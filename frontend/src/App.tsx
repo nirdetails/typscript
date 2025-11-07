3f90711ae51cc73863b6fd69cdbdb0ac3e6c217b
nirdetails <nirdetails@gmail.com>
2025-11-07 10:58:09 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <header>Nissanka</header>
+      <main>indunil</main>
+      <footer>Rammandala</footer>
+    </div>
+  );
+}
+
+export default App;
